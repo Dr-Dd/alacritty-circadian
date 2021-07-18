9547d2486a46c3dd9863c362e03623d05927eb50
drd <idone.leonardo@gmail.com>
2021-07-18 22:51:09 +0200

    Added pretty print

--- install.sh
+++ install.sh
@@ -2,20 +2,23 @@
 
 set -e
 
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
 echo ""
-echo "[!!] Installing ruamel.yaml"
+echo "${blue}[!!]${reset} Installing ruamel.yaml"
 echo ""
 echo pip install -U pip setuptools wheel
 pip install -U pip setuptools wheel
 echo pip install ruamel.yaml
 pip install ruamel.yaml
 echo ""
-echo "[!!] Installing executable scripts, config files and services"
+echo "${blue}[!!]${reset} Installing executable scripts, config files and services"
 echo ""
 echo chmod +x alacritty-circadian.py
 chmod +x alacritty-circadian.py
-echo cp alacritty-circadian.py /usr/local/bin
-cp alacritty-circadian.py /usr/local/bin
+echo cp alacritty-circadian.py /usr/local/bin/
+cp alacritty-circadian.py /usr/local/bin/
 echo cp alacritty-circadian.service $HOME/.config/systemd/user/
 cp alacritty-circadian.service $HOME/.config/systemd/user/
 echo systemctl --user daemon-reload
@@ -24,13 +27,13 @@ echo cp circadian.y*ml $HOME/.config/alacritty/
 cp circadian.y*ml $HOME/.config/alacritty/
 
 echo "
-[!!] alacritty-circadian has been installed.
-[!!]
-[!!] Edit the .yaml file in
-[!!] $HOME/.config/alacritty/alacritty-circadian/circadian.yaml, then
-[!!] start/enable its systemd user service:
-[!!]
-[!!]   systemctl --user enable alacritty-circadian.service
-[!!]   systemctl --user start alacritty-circadian.service
+${blue}[!!]${reset} alacritty-circadian has been installed.
+${blue}[!!]${reset}
+${blue}[!!]${reset} Edit the .yaml file in
+${blue}[!!]${reset} $HOME/.config/alacritty/circadian.y*ml, then
+${blue}[!!]${reset} start/enable its systemd user service:
+${blue}[!!]${reset}
+${blue}[!!]${reset}   systemctl --user enable alacritty-circadian.service
+${blue}[!!]${reset}   systemctl --user start alacritty-circadian.service
 
 "
--- uninstall.sh
+++ uninstall.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
 echo ""
-echo "[!!] Removing all alacritty-circadian files"
+echo "${blue}[!!]${reset} Removing all alacritty-circadian files"
 echo ""
 echo rm /usr/local/bin/alacritty-circadian.py
 rm /usr/local/bin/alacritty-circadian.py
 echo rm $HOME/.config/systemd/user/alacritty-circadian.service
 rm $HOME/.config/systemd/user/alacritty-circadian.service
 echo rm $HOME/.config/alacritty/circadian.y*ml
-rm -r $HOME/.config/alacritty/circadian.y*ml
+rm  $HOME/.config/alacritty/circadian.y*ml
 
 echo "
-[!!] alacritty-circadian has been uninstalled.
-[!!]
-[!!] Disable the running services if you haven't yet:
-[!!]
-[!!]   systemctl --user disable alacritty-circadian.service
-[!!]   systemctl --user stop alacritty-circadian.service
-[!!]
-[!!] pip packages haven't been removed. To uninstall ruamel.yaml, simply run:
-[!!]
-[!!]   pip uninstall ruamel.yaml
+${blue}[!!]${reset} alacritty-circadian has been uninstalled.
+${blue}[!!]${reset}
+${blue}[!!]${reset} Disable the running services if you haven't yet:
+${blue}[!!]${reset}
+${blue}[!!]${reset}   systemctl --user disable alacritty-circadian.service
+${blue}[!!]${reset}   systemctl --user stop alacritty-circadian.service
+${blue}[!!]${reset}
+${blue}[!!]${reset} pip packages haven't been removed. To uninstall ruamel.yaml, simply run:
+${blue}[!!]${reset}
+${blue}[!!]${reset}   pip uninstall ruamel.yaml
 
 "

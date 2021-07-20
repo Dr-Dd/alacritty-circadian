24a400d41d3a2df8565541c08f3434dd55fde063
drd <idone.leonardo@gmail.com>
2021-07-20 12:19:25 +0200

    Added more exception handling, added docstrings, added winpath

--- alacritty_circadian.py
+++ alacritty_circadian.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
-from ruamel.yaml import YAML
+"""
+Small python module for handling the automatic theme switching of alacritty
+via explicit time or phases of the sun
+"""
+
+# Std imports
 from os.path import expandvars
 from pathlib import Path
 from datetime import datetime
 from threading import Timer
-from sys import exit, platform
+import sys
+
+# External imports
+from ruamel.yaml import YAML
 from astral import Observer
 from astral.sun import sun
 
@@ -16,12 +24,15 @@ yaml.default_flow_style = False
 # Set fixed paths
 #
 # Platform dependent paths
-if platform == "linux":
+if sys.platform in ("linux", "darwin", "cygwin"):
     alacritty_path = Path.home() / Path(".config/alacritty")
-elif platform == "win32":
+elif sys.platform == "win32":
     alacritty_path = Path(expandvars("%APPDATA%")) / Path("alacritty")
 config_path = list(alacritty_path.glob("alacritty.y*ml"))[0]
-alacritty_circadian_path = list(alacritty_path.glob("circadian.y*ml"))[0]
+try:
+    alacritty_circadian_path = list(alacritty_path.glob("circadian.y*ml"))[0]
+except IndexError:
+    sys.exit("[ERROR] Circadian config file not found")
 
 times_of_sun = {
     "dawn",
@@ -33,17 +44,27 @@ times_of_sun = {
 
 
 def switch_theme(theme_data, config_data):
-    # No need for truncating
+    """
+    Put theme_data in alacritty's config_data.
+    """
     config_data["colors"] = theme_data["colors"]
     yaml.dump(config_data, config_path)
 
 
 def thread_switch_theme(theme_data, config_data):
+    """
+    Wrapper function to make a thread run switch_theme and exit gracefully.
+    See switch_theme for more info.
+    """
     switch_theme(theme_data, config_data)
-    exit()
+    sys.exit()
 
 
 def get_theme_time(theme, alacritty_circadian_data, now_time):
+    """
+    Get the naive time associated with theme, either from a times_of_sun
+    String or an HH:MM timestamp.
+    """
     theme_time_str = theme["time"]
     if theme_time_str in times_of_sun:
         try:
@@ -51,16 +72,16 @@ def get_theme_time(theme, alacritty_circadian_data, now_time):
                 latitude=alacritty_circadian_data["coordinates"]["latitude"],
                 longitude=alacritty_circadian_data["coordinates"]["longitude"])
         except KeyError:
-            exit("[ERROR] Coordinates not set")
+            sys.exit("[ERROR] Coordinates not set")
         except ValueError:
-            exit("[ERROR] Unable to convert coordinates, check if latitude " +
-                 "and longitude values are set and valid")
+            sys.exit("[ERROR] Unable to convert coordinates, check if " +
+                     "latitude and longitude values are set and valid")
         theme_time = sun(obs)[theme_time_str]
     else:
         try:
             theme_time = datetime.strptime(theme["time"], "%H:%M")
         except ValueError:
-            exit("[ERROR] Unknown time format \"" + theme["time"] + "\"")
+            sys.exit("[ERROR] Unknown time format \"" + theme["time"] + "\"")
     # Convert datetime to naive if aware and fix YY/MM/DD
     theme_time = theme_time.replace(year=now_time.year, month=now_time.month,
                                     day=now_time.day, tzinfo=None)
@@ -69,9 +90,19 @@ def get_theme_time(theme, alacritty_circadian_data, now_time):
 
 def set_appropriate_theme(alacritty_circadian_data, now_time,
                           theme_folder_path, config_data):
+    """
+    Get the nearest neighbor themes list element to now_time and set it as the
+    current theme.
+    """
     # nearest list element neighbor to now_time
     diff = -1
-    for theme in alacritty_circadian_data["themes"]:
+    try:
+        theme_alist = alacritty_circadian_data["themes"]
+    except KeyError:
+        sys.exit("[ERROR] Circadian config theme section not found")
+    if isinstance(theme_alist, type(None)):
+        sys.exit("[ERROR] No themes specified in circadian config")
+    for theme in theme_alist:
         theme_time = get_theme_time(theme, alacritty_circadian_data, now_time)
         switch_time = now_time.replace(hour=theme_time.hour,
                                        minute=theme_time.minute, second=0,
@@ -83,24 +114,25 @@ def set_appropriate_theme(alacritty_circadian_data, now_time,
             preferred_theme = theme
     try:
         theme_path = list(theme_folder_path.glob(preferred_theme["name"]
-                                             + ".y*ml"))[0]
+                                                 + ".y*ml"))[0]
     except IndexError:
-        exit("[ERROR] Unknown theme \"" + preferred_theme["name"]
-             + "\"")
+        sys.exit("[ERROR] Unknown theme \"" + preferred_theme["name"]
+                 + "\"")
     theme_data = yaml.load(theme_path)
     switch_theme(theme_data, config_data)
 
 
 def set_theme_switch_timers():
+    """
+    Parse all yaml files, set a suitable theme and start/restart thread timers
+    for theme switching. The main daemon loop.
+    """
     config_data = yaml.load(config_path)
-    try:
-        alacritty_circadian_data = yaml.load(alacritty_circadian_path)
-        theme_folder_path = Path(expandvars(
-            alacritty_circadian_data["theme-folder"])).expanduser()
-    except:
-        exit("[ERROR] Malformed or missing circadian config file.")
+    alacritty_circadian_data = yaml.load(alacritty_circadian_path)
+    theme_folder_path = Path(expandvars(
+        alacritty_circadian_data["theme-folder"])).expanduser()
     if not theme_folder_path.exists():
-        exit("[ERROR] Path " + str(theme_folder_path) + " not found")
+        sys.exit("[ERROR] Path " + str(theme_folder_path) + " not found")
     set_appropriate_theme(alacritty_circadian_data, datetime.today(),
                           theme_folder_path, config_data)
     # Hot loop
@@ -112,8 +144,8 @@ def set_theme_switch_timers():
                 curr_theme_path = list(theme_folder_path.glob(theme["name"]
                                                               + ".y*ml"))[0]
             except IndexError:
-                exit("[ERROR] Unknown theme \"" + theme["name"]
-                     + "\"")
+                sys.exit("[ERROR] Unknown theme \"" + theme["name"]
+                         + "\"")
             theme_time = get_theme_time(theme, alacritty_circadian_data,
                                         now_time)
             theme_data = yaml.load(curr_theme_path)
@@ -128,9 +160,10 @@ def set_theme_switch_timers():
                                                second=0, microsecond=0)
             delta_t = switch_time - now_time
             seconds = delta_t.seconds + 1
-            t = Timer(seconds, thread_switch_theme, [theme_data, config_data])
-            thread_list.append(t)
-            t.start()
+            timer_thread = Timer(seconds, thread_switch_theme, [theme_data,
+                                                                config_data])
+            thread_list.append(timer_thread)
+            timer_thread.start()
             # Flush stdout to output to log journal
             print("[LOG] Setting up a timer for " + str(theme["name"])
                   + " at: " + str(switch_time), flush=True)
@@ -139,7 +172,9 @@ def set_theme_switch_timers():
 
 
 def main():
-    # .yaml takes precedence over .yml
+    """
+    Entry point
+    """
     set_theme_switch_timers()
 
 

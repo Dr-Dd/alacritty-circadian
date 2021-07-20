5db260baf94b3b1b3416dbed0a43f5f835c19fdc
drd <idone.leonardo@gmail.com>
2021-07-20 15:01:43 +0200

    Prepared Python build package format

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='alacritty-circadian',
+        version='9999.11',
+        author='Leonardo \"drd\" Idone',
+        author_email='idone.leonardo@gmail.com',
+        description='Alacritty time/sun based theme switch daemon',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/Dr-Dd/alacritty-circadian',
+        license_files=('LICENSE'),
+        classifiers=[
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Operating System :: Microsoft :: Windows"
+            ],
+        packages=["alacritty_circadian"],
+        python_requires=">=3.6",
+        install_requires=[ 
+            "ruamel.yaml",
+            "astral"
+        ],
+        entry_points={
+            'console_scripts': [
+                'alacritty-circadian = alacritty_circadian.alacritty_circadian:main',
+            ],
+        },
+        include_package_data=True
+)
+
